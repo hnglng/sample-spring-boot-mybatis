79d988fe76097958a345af7c00736d17e353884d
vhl <huang_liang@ctrip.com>
2017-02-27 11:02:41 +0800

    feat: initial commit

---
+++ sample-spring-boot-mybatis-infrastructure/src/main/java/com/hnglng/sample/spring/boot/mybatis/infrastructure/persistence/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hnglng.sample.spring.boot.mybatis.infrastructure.persistence;
+
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by huang_liang on 2017/2/23.
+ */
+@Repository
+public class UserRepository {
+}
